c5a00dddd27b6fd482ff2da70528b76238c84683
viniciuscarvalho <viniciuscarvalho789@gmail.com>
2016-07-13 22:32:40 -0300

    Changes for load from API Meetup

--- CocoaHeadsApp/CocoaHeadsApp/Classes/Events/Views/EventsListTableViewCell.swift
+++ CocoaHeadsApp/CocoaHeadsApp/Classes/Events/Views/EventsListTableViewCell.swift
@@ -1,8 +1,13 @@
 import UIKit
+import RxSwift
 
 class EventsListTableViewCell: UITableViewCell {
 
     @IBOutlet weak var eventTitleLabel: UILabel!
+    @IBOutlet weak var eventDateTimeLabel: UILabel!
+    @IBOutlet weak var eventDateLabel: UILabel!
+    
+    let disposeBar = DisposeBag()
  
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -11,7 +16,7 @@ class EventsListTableViewCell: UITableViewCell {
         
     }
     
-    let event = Dynamic<Event?>(nil)
+    let event = Variable<Event?>(nil)
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
@@ -19,8 +24,12 @@ class EventsListTableViewCell: UITableViewCell {
     }
     
     private func commonInit() {
-        self.event.bind(self) { event in
+        self.event.asObservable().subscribeNext { [weak self] (event) in
             self.eventTitleLabel.text = event?.name ?? "Indisponível"
+            self.eventDateLabel.text = event?.eventDateTempo.format("dd 'de' MMMM 'de' YYYY")
+            self.eventDateTimeLabel.text = event?.DateTempo.format("HH:mm")
+            }.addDisposableTo(self.dispoeBag)
+        
         }
     }
     
@@ -36,11 +45,10 @@ class EventsListTableViewCell: UITableViewCell {
             accessoryType = .None
         }
     
-    
     }
     
     override func prepareForReuse() {
         super.prepareForReuse()
         self.event.value = nil
     }
-}
+
